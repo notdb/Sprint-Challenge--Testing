2c7fb805d8a4043901c7aa0233e488eb780f5863
Doug <me@douglasbowen.com>
2019-06-21 12:41:36 -0400

    add two tests for post endpoint on /games

--- api/server.spec.js
+++ api/server.spec.js
@@ -18,4 +18,25 @@ describe("server", () => {
         .expect(200);
     });
   });
+
+  describe("post /games", () => {
+    it("responds with 201 OK and the game added", () => {
+      let game = {
+        title: "Dota Underlords",
+        genre: "Auto Chess",
+        releaseYear: 2019
+      };
+      return supertest(server)
+        .post("/games", game)
+        .expect(201);
+    });
+    it("responds with 422 because no title", () => {
+      let game = {
+        genre: "Auto Chess",
+        releaseYear: 2019
+      };
+      return supertest(server).post("/games", game);
+      expect(422);
+    });
+  });
 });
