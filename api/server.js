4de4fa756235badd6740bd971d08dc75139e42ed
Doug <me@douglasbowen.com>
2019-06-21 12:50:53 -0400

    add post endpoint for /games

--- api/server.js
+++ api/server.js
@@ -18,4 +18,14 @@ server.get("/games", (req, res) => {
   res.status(200).json(gamesArray);
 });
 
+server.post("/games", (req, res) => {
+  let newGame = req.body;
+  if (!newGame.title || !newGame.genre) {
+    res.status(422).json({ message: "Game title and body is required" });
+  } else {
+    games.push(newGame);
+    res.status(201).json(newGame);
+  }
+});
+
 module.exports = server;
